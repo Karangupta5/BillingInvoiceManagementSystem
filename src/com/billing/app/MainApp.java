9c95dfad2904c9aeca70d5e8fb0af8f89d4e80aa
Karan <karangupta0503@gmail.com>
2025-11-17 17:30:32 +0530

    Designed the layout of class and packages

---
+++ src/com/billing/app/MainApp.java
@@ -0,0 +1,9 @@
+package com.billing.app;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/billing/interfaces/Taxable.java
@@ -0,0 +1,5 @@
+package com.billing.interfaces;
+
+public class Taxable {
+
+}
